df1664c210153d2ba69bc079071f2ac560a95af1
kingnan <373734199@qq.com>
2024-02-24 20:31:32 +0800

    更新代码

---
+++ FreeRTOS/Demo/STM32_keil/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "FreeRTOS.h"
+
+int main(){
+    printf("kingnan ");
+    return 0;
+}
---
+++ FreeRTOS/Demo/esp8266/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+// #include "FreeRTOS.h"
+#include "FreeRTOSConfig.h"
+// FreeRTOS/Source/include/FreeRTOS.h
+#include "../..//Source/include/FreeRTOS.h"
+// #include "FreeRTOS.h";
+// #include "task.h";
+// #include "queue.h";
+
+
+int main(){
+    printf("kingnan ");
+    return 0;
+}
